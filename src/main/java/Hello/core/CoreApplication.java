ac547aabd64da46b7669989f3e5d1d54b8abebc5
Hyeri Jang <chjang5621@naver.com>
2021-08-21 20:30:28 +0900

    관심사의 분리

--- src/main/java/Hello/core/CoreApplication.java
+++ src/main/java/Hello/core/CoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
 	public static void main(String[] args) {
+		AppConfig appConfig = new AppConfig();
 		SpringApplication.run(CoreApplication.class, args);
 	}
 

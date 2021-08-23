ded19749544b1d6b710a3cb1da13cc0321b101f2
Hyeri Jang <chjang5621@naver.com>
2021-08-23 17:55:59 +0900

    AppConfig 리팩터링
    new MemoryMemberRepository()의 중복 제거

--- src/main/java/Hello/core/AppConfig.java
+++ src/main/java/Hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package Hello.core;
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
+import Hello.core.member.MemberRepository;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
 import Hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import Hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+    }
 }

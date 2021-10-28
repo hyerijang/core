fbca81d0d57febc85d9d9f79a3d3a755bda0388a
Hyeri Jang <chjang5621@naver.com>
2021-10-28 12:00:07 +0900

    9. 빈 스코프 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결
    	1. 스프링 컨테이너에 요청
    	2. ObjectFactory, ObjectProvider
    	3. javax의 provider 라이브러리 사용

---
+++ src/test/java/Hello/core/scope/SingletonWithPrototypeTestWithObjectProvider.java
@@ -0,0 +1,87 @@
+package Hello.core.scope;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTestWithObjectProvider {
+
+    @Test
+    void prototypeBeanFind() {
+        // given
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        // when
+        prototypeBean1.addCount();
+        // then
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+        assertThat(prototypeBean2.getCount()).isEqualTo(0);
+        assertThat(prototypeBean1.getCount()).isNotEqualTo(prototypeBean2.getCount());
+    }
+
+    @Test
+    @DisplayName("싱글톤 빈에서 프로토타입 빈 사용시 ObjectProvider 이용")
+    void singletonClientUsePrototype() {
+        // given
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        // when
+        // ClientBean: 싱글톤
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        // then
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+}

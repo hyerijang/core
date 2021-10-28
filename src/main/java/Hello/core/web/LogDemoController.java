44df925502681401a57e5f155517ac244ceaeb7d
Hyeri Jang <chjang5621@naver.com>
2021-10-28 16:28:43 +0900

    9. 빈 스코프 - request 스코프 - request 스코프 예제 만들기
    오류가 발생하는 코드 (실행되지 않음)

---
+++ src/main/java/Hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package Hello.core.web;
+
+import Hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    // Error creating bean with name 'myLogger': Scope 'request' is not active for the current thread;
+    private final MyLogger myLogger; //scope 의 범위가 "request"이므로 의존관계 주입 시 오류 발생
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+
+}

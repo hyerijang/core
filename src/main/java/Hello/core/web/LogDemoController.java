fefa7f47275dd8f529bd7e21026de88b1d19ea1f
Hyeri Jang <chjang5621@naver.com>
2021-10-28 16:38:35 +0900

    9. 빈 스코프 - request 스코프 - 스코프와 Provider

--- src/main/java/Hello/core/web/LogDemoController.java
+++ src/main/java/Hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package Hello.core.web;
 
 import Hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +15,17 @@ public class LogDemoController {
 
     private final LogDemoService logDemoService;
     // Error creating bean with name 'myLogger': Scope 'request' is not active for the current thread;
-    private final MyLogger myLogger; //scope 의 범위가 "request"이므로 의존관계 주입 시 오류 발생
+    private final ObjectProvider<MyLogger> myLoggerProvider; //scope 의 범위가 "request"이므로 의존관계 주입 시 오류 발생
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
